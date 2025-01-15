191fc0c113d3821017ddfc1dc70e144a3ef5b230
ArleiDev <arleicodedev@gmail.com>
2025-01-15 12:44:47 -0300

    .

--- src/components/EncontreoCartaoPerfeitoParaVoce.tsx
+++ src/components/EncontreoCartaoPerfeitoParaVoce.tsx
@@ -8,26 +8,13 @@ import card3 from '@/assets/imgs/card3.png'
 import TiltCard from '@/components/ui/TiltCard'
 const EncontreoCartaoPerfeitoParaVoce = () => {
 
-    useEffect(() => {
-        const sr = ScrollReveal({
-          origin: 'bottom', // Origem da animação
-          distance: '50px', // Distância do movimento
-          duration: 1000,   // Duração da animação em milissegundos
-          delay: 200,       // Atraso antes de iniciar
-          reset: false,      // Repetir animação ao voltar ao scroll
-        });
-    
-        sr.reveal('.reveal', {
-          interval: 200, // Intervalo entre os elementos animados
-        });
-      }, []);
-
+ 
 
   return (
     <div className="bg-black flex items-center justify-center py-10 text-white relative">
       <div className="flex flex-col gap-40">
         <div>
-          <h1 className='reveal text-4xl max-w-[1000px] text-center mt-20 mb-5 items-center '>
+          <h1 className=' text-4xl max-w-[1000px] text-center mt-20 mb-5 items-center '>
           Conquiste a liberdade financeira que você merece. Nossos cartões oferecem 
           benefícios excepcionais, recompensas exclusivas e muito mais.
           </h1>
