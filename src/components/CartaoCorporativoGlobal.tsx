2b45f6e8a7051e06268ba67212a0eb1484133566
ArleiDev <arleicodedev@gmail.com>
2025-01-15 16:14:35 -0300

    feat

--- src/components/CartaoCorporativoGlobal.tsx
+++ src/components/CartaoCorporativoGlobal.tsx
@@ -1,8 +1,11 @@
-
+import { TextAnimate } from "./ui/text-animate"
 const CartaoCorporativoGlobal = () => {
   return (
-    <div>
-      CartaoCorporativoGlobal
+    <div className="flex flex-col items-center justify-center bg-black text-green-500">
+      <TextAnimate className="text-6xl text-center max-w-[1200px] mb-2">Gerencie seus gastos globalmente com cartões ilimitados
+
+</TextAnimate>
+<TextAnimate  className="text-6xl text-white text-center max-w-[1000px] mb-10 font-bold"> e total visibilidade.</TextAnimate>
     </div>
   )
 }
